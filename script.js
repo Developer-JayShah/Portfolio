663859f24690605405666892de74547bce7ab99b
jayshah1234 <59352098+jayshah1234@users.noreply.github.com>
2021-03-29 17:03:32 +0530

    new

---
+++ script.js
@@ -0,0 +1,29 @@
+$(function () {
+	var filterList = {
+		init: function () {
+			// MixItUp plugin
+			// http://mixitup.io
+			$('.portfolio-grid').mixItUp({
+				selectors: {
+  			  target: '.portfolio',
+  			  filter: '.filter'	
+  		  },
+  		  load: {
+    		  filter: 'all' // show app tab on first load
+    		}     
+			});								
+		}
+	};
+	// Run the show!
+	filterList.init();
+});	
+
+
+
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
